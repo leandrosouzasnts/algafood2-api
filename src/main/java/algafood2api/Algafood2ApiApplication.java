7e548dfea4d5f23a27719d791c5c87e1a6410135
leandrosouzasnts <1eandrosouzasnts@gmail.com>
2024-04-14 13:20:55 -0300

    Initial commit

---
+++ src/main/java/algafood2api/Algafood2ApiApplication.java
@@ -0,0 +1,13 @@
+package algafood2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algafood2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Algafood2ApiApplication.class, args);
+	}
+
+}
