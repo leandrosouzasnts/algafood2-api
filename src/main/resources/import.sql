ffab99fa9011497a2488b640b26dee539753d265
leandrosouzasnts <1eandrosouzasnts@gmail.com>
2024-04-14 23:38:24 -0300

    feat: adiciona import.sql para alimentar tabelas do banco

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO algafood2.cozinha (nome) VALUES('Mineira');
+INSERT INTO algafood2.cozinha (nome) VALUES('Paulista');
+INSERT INTO algafood2.cozinha (nome) VALUES('Gaucha');
+INSERT INTO algafood2.cozinha (nome) VALUES('Londrina');
+INSERT INTO algafood2.cozinha (nome) VALUES('Americana');
+INSERT INTO algafood2.cozinha (nome) VALUES('Brasileira');
